9e8bbc0285ac737fc9e30f049d9e581eaf3cfa7c
Cris Jr <cristianoandre96@gmail.com>
2022-04-01 16:28:50 -0300

    Creating the components structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent,
+    HeaderComponent,
+    RecipesComponent
   ],
   imports: [
     BrowserModule
