1e51824e3df1e1600a7e815b192e96d8bc638f83
Cris Jr <cristianoandre96@gmail.com>
2022-04-08 12:29:13 -0300

    Adding click events to RecipeItems

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Recipe } from '../../recipe.model';
 
 @Component({
@@ -9,10 +9,14 @@ import { Recipe } from '../../recipe.model';
 export class RecipeItemComponent implements OnInit {
 
   @Input('srvRecipe') recipe: Recipe;
+  @Output() recipeClicked = new EventEmitter<void>();
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onClicked() {
+    this.recipeClicked.emit();
+  }
 }
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, EventEmitter, OnInit, Output } from "@angular/core";
 import { Recipe } from "../recipe.model";
 
 @Component({
@@ -8,6 +8,8 @@ import { Recipe } from "../recipe.model";
 })
 export class RecipeListComponent implements OnInit{
 
+  @Output() clickedRecipe = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [
     new Recipe('Chris', 'The great', 'https://images.pexels.com/photos/11143927/pexels-photo-11143927.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260'),
     new Recipe('And', 'The goat', 'https://images.pexels.com/photos/7609840/pexels-photo-7609840.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940')
@@ -16,6 +18,10 @@ export class RecipeListComponent implements OnInit{
   constructor(){}
 
   ngOnInit(): void {
+  }
+
+  onDisplay(recipe: Recipe) {
+    this.clickedRecipe.emit(recipe);
   }
 
 }
