cdcb2267f4e3794956ae556a664ca2e54c9abf5c
Cris Jr <cristianoandre96@gmail.com>
2022-04-08 09:07:56 -0300

    Implementing the Header navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'shopinho-project';
+
+  loadedFeature = 'recipe';
+
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
+  }
 }
