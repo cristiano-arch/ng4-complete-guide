95ddec068978b6ecda21a4c1b6c471e5931e19a5
Cris Jr <cristianoandre96@gmail.com>
2022-06-22 21:00:30 -0300

    Deleting a single Ingredient

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -35,4 +35,8 @@ export class ShoppingListService {
     this.ingredientsChanged.next(this.ingredients.slice());
   }
 
+  deleteIngredient(index: number) {
+    this.ingredients.splice(index, 1);
+    this.ingredientsChanged.next(this.ingredients.slice());
+  }
 }
