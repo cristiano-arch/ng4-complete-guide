cb9a50837c31165b9ff6e1a5a0f6f8fdaf44e53f
Cris Jr <cristianoandre96@gmail.com>
2022-08-25 16:03:02 -0300

    Sending Login Requests

--- src/app/recipes/auth/auth.service.ts
+++ src/app/recipes/auth/auth.service.ts
@@ -2,21 +2,22 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { catchError, throwError } from "rxjs";
 
-interface AuthResponseData {
+export interface AuthResponseData {
   kind: string,
   idToken: string,
   email: string,
   refrehToken: string,
   expiresIn: string,
-  localId: string
+  localId: string,
+  registered?: boolean;
 }
 
+/* DO NOT EXPOSE YOUR Web API Key HERE!!! BECAUSE THIS IS A PUBLIC REPOSITORY ON GITHUB!!! */
 @Injectable({providedIn: 'root'})
 export class AuthService {
 
   constructor(private httpClient: HttpClient) {}
 
-  /* DO NOT EXPOSE YOUR Web API Key HERE!!! BECAUSE THIS IS A PUBLIC REPOSITORY ON GITHUB!!! */
   signup(email: string, password: string) {
     return this.httpClient.post<AuthResponseData>(
       'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=[API_KEY]',
@@ -37,4 +38,16 @@ export class AuthService {
       return throwError(errorMessage);
     }));
   }
+
+  login (email: string, password: string) {
+    return this.httpClient.post<AuthResponseData>(
+      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=[API_KEY]',
+      {
+        email: email,
+        password: password,
+        returnSecureToken: true
+      }
+    );
+  }
+
 }
