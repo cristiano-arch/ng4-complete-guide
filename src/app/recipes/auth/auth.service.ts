ed451cabc1df172e39c00ccd53e04f53468c9e5d
Cris Jr <cristianoandre96@gmail.com>
2022-08-26 08:47:51 -0300

    Login Error Handling

--- src/app/recipes/auth/auth.service.ts
+++ src/app/recipes/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { catchError, throwError } from "rxjs";
 
@@ -26,17 +26,7 @@ export class AuthService {
         password: password,
         returnSecureToken: true
       }
-    ).pipe(catchError(errorRes => {
-      let errorMessage = 'An unknown error occurred!';
-      if (!errorRes.error || !errorRes.error.error) {
-        return throwError(errorMessage);
-      }
-      switch (errorRes.error.error.message) {
-        case 'EMAIL_EXISTS':
-          errorMessage = 'This email exists already!';
-      }
-      return throwError(errorMessage);
-    }));
+    ).pipe(catchError(this.handleError));
   }
 
   login (email: string, password: string) {
@@ -47,7 +37,25 @@ export class AuthService {
         password: password,
         returnSecureToken: true
       }
-    );
+    ).pipe(catchError(this.handleError));
+  }
+
+  private handleError (errorRes: HttpErrorResponse) {
+    let errorMessage = 'An unknown error occurred!';
+    if (!errorRes.error || !errorRes.error.error) {
+      return throwError(errorMessage);
+    }
+    switch (errorRes.error.error.message) {
+      case 'EMAIL_EXISTS':
+        errorMessage = 'This email exists already!';
+        break;
+      case 'EMAIL_NOT_FOUND':
+        errorMessage = 'This email does not exists.';
+        break;
+      case 'INVALID_PASSWORD':
+        errorMessage = 'This password is not correct.'
+    }
+    return throwError(errorMessage);
   }
 
 }
