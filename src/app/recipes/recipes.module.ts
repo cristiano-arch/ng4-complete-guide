e3ff801796fa082082be52ebfcda8ea3e87c8dcd
Cris Jr <cristianoandre96@gmail.com>
2022-10-19 08:54:13 -0300

    Component Declarations

--- src/app/recipes/recipes.module.ts
+++ src/app/recipes/recipes.module.ts
@@ -24,14 +24,6 @@ import { RecipesComponent } from "./recipes.component";
     CommonModule,
     ReactiveFormsModule,
     RecipesRoutingModule
-  ],
-  exports: [
-    RecipeListComponent,
-    RecipeItemComponent,
-    RecipeDetailComponent,
-    RecipesComponent,
-    RecipeStartComponent,
-    RecipeEditComponent
   ]
 })
 export class RecipesModule {
