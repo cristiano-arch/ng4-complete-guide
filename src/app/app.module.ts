d347a38d83e922a4b847404a92499f2c89d6bc62
Cris Jr <cristianoandre96@gmail.com>
2022-10-23 05:38:31 -0300

    Adding an Auth Feature Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,9 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AuthComponent } from './auth/auth.component';
+import { AuthModule } from './auth/auth.module';
 import { CoreModule } from './core.module';
 import { HeaderComponent } from './header/header.component';
 import { RecipesModule } from './recipes/recipes.module';
@@ -12,22 +11,17 @@ import { SharedModule } from './shared/shared.module';
 import { ShoppingListModule } from './shopping-list/shoping-list.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    AuthComponent
-  ],
+  declarations: [AppComponent, HeaderComponent],
   imports: [
     BrowserModule,
-    FormsModule,
-    ReactiveFormsModule,
     HttpClientModule,
     AppRoutingModule,
     RecipesModule,
     ShoppingListModule,
     SharedModule,
-    CoreModule
+    CoreModule,
+    AuthModule,
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,24 @@
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { RouterModule } from "@angular/router";
+import { SharedModule } from "../shared/shared.module";
+import { AuthComponent } from "./auth.component";
+
+@NgModule({
+  declarations: [
+    AuthComponent
+  ],
+  imports: [
+    RouterModule.forChild([{ path: 'auth', component: AuthComponent }]),
+    CommonModule,
+    FormsModule,
+    SharedModule
+  ],
+  exports: [
+    AuthComponent
+  ]
+})
+export class AuthModule {
+
+}
